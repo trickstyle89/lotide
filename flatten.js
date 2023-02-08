5c545218a4cedd578706308b531aaddaa5b7b396
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-08 08:50:39 -0500

    Create flatten.js

---
+++ flatten.js
@@ -0,0 +1,23 @@
+const [node, script, array] = process.argv;
+
+let arraySample = [
+    [1, 2],
+    [3, 4],
+    [5, 6],[7, 8, 9],
+    [10, 11, 12, 13, 14, 15]
+];
+
+function flatten(arraySample) {
+    let flatArray = [].concat(...arraySample);
+    // let flatArray = [].concat.apply([], arraySample); another alternative
+    //flat() works too!
+    return flatArray;
+}
+
+console.log(flatten(arraySample));
+
+/* below is supposely another method but did not work. 
+let flatArray = [].concat.apply([], arraySample);
+let flatArray = [].concat(...arr);
+flat()
+*/
