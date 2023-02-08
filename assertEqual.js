a259874135b5cb8124498a058835fb353bb64865
Ivan Chew <120282621+trickstyle89@users.noreply.github.com>
2023-02-08 16:41:07 -0500

    day 3

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,9 @@
 const assertEqual = function(actual, expected) {
   const devilEmoji = "\uD83D\uDC7B";
   const angelEmoji = "\uD83D\uDE07";
-
-  if (actual === expected) {
+  
+  if (JSON.stringify(actual) === JSON.stringify(expected)) {
     console.log(`${angelEmoji} Assertion Passed: ${actual} === ${expected} ${angelEmoji}`);
-  }
-  console.log(`${devilEmoji} Assertion Failed: ${actual} !== ${expected} ${devilEmoji}`);
+  } else
+    console.log(`${devilEmoji} Assertion Failed: ${actual} !== ${expected} ${devilEmoji}`);
 };
-
-assertEqual("Lighthouse Labs", "Bootcamp");
--- countLetters.js
+++ countLetters.js
@@ -1,1 +1,24 @@
-
+const assertEqual = function(actual, expected) {
+    const devilEmoji = "\uD83D\uDC7B";
+    const angelEmoji = "\uD83D\uDE07";
+    
+    if (JSON.stringify(actual) === JSON.stringify(expected)) {
+      console.log(`${angelEmoji} Assertion Passed: ${actual} === ${expected} ${angelEmoji}`);
+    } else
+      console.log(`${devilEmoji} Assertion Failed: ${actual} !== ${expected} ${devilEmoji}`);
+  };
+  
+  
+  function countOnly(names, options) {
+    let results = {};
+    for (const name of names) {
+      if (options[name]) {
+        if (results[name]) {
+          results[name] += 1;
+        } else {
+          results[name] = 1;
+        }
+      }
+    }
+    return results;
+  }
